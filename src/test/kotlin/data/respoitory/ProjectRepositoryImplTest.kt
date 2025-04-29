e2ed8c0528917ab0b283923fa58e0ec2218fc07f
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-04-29 18:59:12 +0300

    add all test cases for getProjects in ProjectRepositoryImplTest.kt add all test cases for getProjectById in ProjectRepositoryImplTest.kt
    Took 15 seconds

--- src/test/kotlin/data/respoitory/ProjectRepositoryImplTest.kt
+++ src/test/kotlin/data/respoitory/ProjectRepositoryImplTest.kt
@@ -1,13 +1,16 @@
 package data.respoitory
 
 import com.google.common.truth.Truth.assertThat
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.datetime.LocalDateTime
 import org.example.data.respoitory.ProjectRepositoryImpl
 import org.example.data.storge.audit.AuditDataSource
 import org.example.data.storge.project.ProjectDataSource
+import org.example.domain.model.EiffelFlowException
 import org.example.domain.model.entities.Project
 import org.example.domain.model.entities.State
+import org.example.domain.repository.ProjectRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.UUID
@@ -15,7 +18,7 @@ import java.util.UUID
 //todo change all of the test
 class ProjectRepositoryImplTest {
 
-    private lateinit var projectRepository: ProjectRepositoryImpl
+    private lateinit var projectRepository: ProjectRepository
     private val projectDataSource: ProjectDataSource = mockk()
     private val auditDataSource: AuditDataSource = mockk()
 
@@ -75,20 +78,82 @@ class ProjectRepositoryImplTest {
     }
 
     @Test
-    fun `getProjects should return list of projects`() {
+    fun `should return Result of empty list of Projects when there is no project in data source`() {
+        //Given
+        every { projectDataSource.getProjects() } returns Result.success(emptyList())
+
+        // When / Then
         try {
-            projectRepository.getProjects()
+            val result = projectRepository.getProjects()
         } catch (e: NotImplementedError) {
             assertThat(e.message).contains("Not yet implemented")
         }
     }
 
     @Test
-    fun `getProjectById should return list of projects`() {
+    fun `should return Result of Projects when at least one project exists in data source`() {
+        //Given
+        every { projectDataSource.getProjects() } returns Result.success(listOf(PROJECT))
+
+        // When / Then
         try {
-            projectRepository.getProjectById(UUID.randomUUID())
+            val result = projectRepository.getProjects()
         } catch (e: NotImplementedError) {
             assertThat(e.message).contains("Not yet implemented")
         }
+    }
+
+    @Test
+    fun `should return Result of ElementNotFoundException when project doesn't exists in data source`() {
+        //Given
+        val exception = EiffelFlowException.ElementNotFoundException("Project not found")
+        every { projectDataSource.getProjects() } returns Result.failure(exception)
+
+        // When / Then
+        try {
+            val result = projectRepository.getProjects()
+        } catch (e: NotImplementedError) {
+            assertThat(e.message).contains("Not yet implemented")
+        }
+    }
+
+    @Test
+    fun `should return Result of Project when the given Id match project record exists in data source`() {
+        //Given
+        every { projectDataSource.getProjectById(PROJECT.projectId) } returns Result.success(PROJECT)
+
+        // When / Then
+        try {
+            val result = projectRepository.getProjectById(PROJECT.projectId)
+        } catch (e: NotImplementedError) {
+            assertThat(e.message).contains("Not yet implemented")
+        }
+    }
+
+    @Test
+    fun `should return Result of ElementNotFoundException when searching for project doesn't exists in data source`() {
+        val exception = EiffelFlowException.ElementNotFoundException("Project not found")
+        every { projectDataSource.getProjectById(UUID.randomUUID()) } returns Result.failure(exception)
+
+        // When / Then
+        try {
+            val result =  projectRepository.getProjectById(UUID.randomUUID())
+        } catch (e: NotImplementedError) {
+            assertThat(e.message).contains("Not yet implemented")
+        }
+    }
+
+    companion object {
+        private val PROJECT = Project(
+            projectId = UUID.fromString("02ad4499-5d4c-4450-8fd1-8294f1bb5748"),
+            projectName = "Project1",
+            projectDescription = "Description1",
+            createdAt = LocalDateTime.parse("1999-08-07T22:22:22"),
+            adminId = UUID.fromString("02ad4499-5d4c-4450-8fd1-8294f1bb5741"),
+            states = emptyList()
+        )
+
+        private const val CSV_STRING_LINE =
+            "02ad4499-5d4c-4450-8fd1-8294f1bb5748,Project1,Description1,1999-08-07T22:22:22,02ad4499-5d4c-4450-8fd1-8294f1bb5741"
     }
 }
