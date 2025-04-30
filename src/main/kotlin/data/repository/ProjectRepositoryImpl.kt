660ecd2496c2ba35dcf4a28f4e23b13fd0c0d66b
hossam haroon <hosamharon2016@gmail.com>
2025-04-30 19:25:30 +0300

    add deleteProject function in ProjectRepositoryImpl

--- src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
@@ -1,7 +1,12 @@
 package org.example.data.repository
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.example.data.storage.audit.AuditDataSource
 import org.example.data.storage.project.ProjectDataSource
+import org.example.domain.model.entities.AuditAction
+import org.example.domain.model.entities.AuditLog
 import org.example.domain.model.entities.Project
 import org.example.domain.repository.ProjectRepository
 import java.util.UUID
@@ -20,7 +25,35 @@ class ProjectRepositoryImpl(
     }
 
     override fun deleteProject(projectId: UUID): Result<Project> {
-        TODO("Not yet implemented")
+        val deletedProject = projectDataSource.deleteProject(projectId)
+
+        return deletedProject.fold(
+            onSuccess = {project->
+                val auditLog = AuditLog(
+                    auditId = UUID.randomUUID(),
+                    itemId = project.projectId,
+                    itemName = project.projectName,
+                    userId = project.adminId,
+                    userName = "Admin",
+                    actionType = AuditAction.DELETE,
+                    auditTime = currentTime,
+                    changedField = null,
+                    oldValue = null,
+                    newValue = project.projectName
+                )
+                return auditDataSource.createAuditLog(auditLog).fold(
+                    onSuccess = {
+                        Result.success(project)
+                    },
+                    onFailure = {
+                        Result.failure(it)
+                    }
+                )
+            },
+            onFailure = {
+                Result.failure(it)
+            }
+        )
     }
 
     override fun getProjectById(projectID: UUID): Result<Project> {
@@ -29,5 +62,9 @@ class ProjectRepositoryImpl(
 
     override fun getProjects(): Result<List<Project>> {
         TODO("Not yet implemented")
+    }
+
+    companion object{
+        private val currentTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
     }
 }
