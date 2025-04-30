fd8bf87f89add22386ab404707bcef2b2821a6e3
Mohamed Elhanafy <m.elhanafy@otas.net>
2025-04-30 10:40:52 +0300

    add edit task use case tests and exception handling for task not found, line not found, and no changes detected scenarios

--- src/main/kotlin/domain/model/exception/EiffelFlowException.kt
+++ src/main/kotlin/domain/model/exception/EiffelFlowException.kt
@@ -20,4 +20,9 @@ abstract class EiffelFlowException(message: String?) : Exception(message) {
     class LineNotFoundException(line: String? = null) :
         EiffelFlowException("Line not found $line")
 
+    class TaskNotFoundException(message: String? = null) :
+        EiffelFlowException(message ?: "Task not found")
+
+    class NoChangesException(message: String? = null) :
+        EiffelFlowException(message ?: "No changes detected")
 }
