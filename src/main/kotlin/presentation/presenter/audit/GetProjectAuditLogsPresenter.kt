5684d3fb9e62576437383967915a3716dac03216
Mahmoud Tarek <dev.mahmoudtarek@gmail.com>
2025-05-02 15:06:07 +0300

    implement GetProjectAuditLogsPresenter.kt

--- src/main/kotlin/presentation/presenter/audit/GetProjectAuditLogsPresenter.kt
+++ src/main/kotlin/presentation/presenter/audit/GetProjectAuditLogsPresenter.kt
@@ -2,12 +2,25 @@ package org.example.presentation.presenter.audit
 
 import org.example.domain.model.AuditLog
 import org.example.domain.usecase.audit.GetProjectAuditUseCase
+import org.example.presentation.view.audit.GetProjectAuditLogsCLI
 import java.util.UUID
 
 class GetProjectAuditLogsPresenter(
     private val getProjectAuditUseCase: GetProjectAuditUseCase
 ) {
     fun getProjectAuditLogsById(auditId: UUID): Result<List<AuditLog>> {
-        TODO("Not yet implemented")
+        val projectAuditLogs = getProjectAuditUseCase.getProjectAuditLogsById(auditId)
+        return projectAuditLogs.fold(
+            onSuccess = { logs ->
+                if (logs.isEmpty()) {
+                    Result.failure(Exception("No audit logs found for this project."))
+                } else {
+                    Result.success(logs)
+                }
+            },
+            onFailure = { error ->
+                Result.failure(Exception("Failed to load logs: ${error.message}"))
+            }
+        )
     }
 }
