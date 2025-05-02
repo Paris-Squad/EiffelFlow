fec48a08a50148633b2934518c6d72ee76f05bc8
hossam haroon <hosamharon2016@gmail.com>
2025-05-02 23:08:26 +0300

    add test cases for DeleteProjectCLI

---
+++ src/main/kotlin/presentation/view/project/DeleteProjectCLI.kt
@@ -0,0 +1,11 @@
+package org.example.presentation.view.project
+
+import org.example.presentation.presenter.project.DeleteProjectPresenter
+
+class DeleteProjectCLI(
+    private val deleteProjectPresenter: DeleteProjectPresenter
+) {
+    operator fun invoke(){
+        TODO("Not yet implemented")
+    }
+}
