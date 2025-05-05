2f0cf527bafc29774346084f8b4ffbb7a1e7f99b
aliYasser <ay6516169@gmail.com>
2025-05-05 12:50:42 +0200

    rename class ViewTaskHistory to ViewTaskHistoryUseCase

---
+++ src/main/kotlin/domain/usecase/task/ViewTaskHistoryUseCase.kt
@@ -0,0 +1,11 @@
+package org.example.domain.usecase.task
+
+import org.example.domain.model.AuditLog
+import org.example.domain.repository.AuditRepository
+import java.util.*
+
+
+class ViewTaskHistoryUseCase (private val auditRepository: AuditRepository) {
+
+    fun viewTaskHistory(taskId: UUID): Result<List<AuditLog>> = auditRepository.getTaskAuditLogById(taskId)
+}   
