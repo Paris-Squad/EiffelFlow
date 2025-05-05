ccc68fbbe6503d5a5f26518c18c11a18106ab2cd
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-05-05 07:07:59 +0300

    Refactor AuthRepositoryImpl and remove runCatching and change the return type from Result and fix the corresponding tests

--- src/main/kotlin/data/repository/AuthRepositoryImpl.kt
+++ src/main/kotlin/data/repository/AuthRepositoryImpl.kt
@@ -10,46 +10,45 @@ import org.example.domain.utils.ValidationErrorMessage
 import java.io.FileNotFoundException
 
 class AuthRepositoryImpl(
-    private val fileDataSource: FileDataSource,
-    private val userCsvParser: UserCsvParser
+    private val fileDataSource: FileDataSource, private val userCsvParser: UserCsvParser
 ) : AuthRepository {
-    override fun loginUser(username: String, password: String): Result<String> {
-        return runCatching {
-            val lines = fileDataSource.readLinesFromFile()
-            val users = lines
-                .filter { it.isNotBlank() }
-                .map { userCsvParser.parseCsvLine(it) }
 
-            val user = users.find { it.username == username }
-                ?: throw EiffelFlowException.AuthenticationException(setOf(ValidationErrorMessage.INVALID_USERNAME))
+    override fun loginUser(username: String, password: String): User {
+        return try {
+            val lines = fileDataSource.readLinesFromFile()
+            val users = lines.filter { it.isNotBlank() }.map { userCsvParser.parseCsvLine(it) }
+
+            val user = users.find { it.username == username } ?: throw EiffelFlowException.AuthenticationException(
+                setOf(ValidationErrorMessage.INVALID_USERNAME)
+            )
 
             if (user.password != password) {
                 throw EiffelFlowException.AuthenticationException(setOf(ValidationErrorMessage.INVALID_PASSWORD))
             }
 
             saveUserLogin(user)
-            "Login successfully"
-        }.recoverCatching {
-            when (it) {
-                is EiffelFlowException -> throw it
-                else -> throw EiffelFlowException.IOException(it.message)
+        } catch (e: Exception) {
+            when (e) {
+                is EiffelFlowException -> throw e
+                else -> throw EiffelFlowException.IOException(e.message)
             }
         }
+
     }
 
-    override fun saveUserLogin(user: User): Result<User> {
-        return runCatching {
+    override fun saveUserLogin(user: User): User {
+        return try {
             val userCsv = userCsvParser.serialize(user)
             fileDataSource.writeLinesToFile(userCsv)
             SessionManger.login(user)
             user
-        }.recoverCatching {
-            throw EiffelFlowException.IOException(it.message)
+        } catch (e: Exception) {
+            throw EiffelFlowException.IOException(e.message)
         }
     }
 
-    override fun isUserLoggedIn(): Result<Boolean> {
-        return runCatching {
+    override fun isUserLoggedIn(): Boolean {
+        return try {
             val lines = fileDataSource.readLinesFromFile()
             if (lines.any { it.isNotBlank() }) {
                 val userCsv = lines.first { it.isNotBlank() }
@@ -59,19 +58,18 @@ class AuthRepositoryImpl(
             } else {
                 false
             }
-        }.recoverCatching { e ->
-            when (e) {
-                is FileNotFoundException -> false
-                else -> throw e
-            }
+        } catch (e: Exception) {
+            if (e is FileNotFoundException) return false
+            throw EiffelFlowException.IOException(e.message)
         }
     }
 
-    override fun clearLogin(): Result<Boolean> {
-        return runCatching {
+    override fun clearLogin() {
+        return try {
             fileDataSource.clearFile()
             SessionManger.logout()
-            true
+        } catch (e: Exception) {
+            throw EiffelFlowException.IOException(e.message)
         }
     }
 
