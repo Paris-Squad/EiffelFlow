2fd47f692c316c0227b46f927b6a370f2049a9ce
dina othman <dinaosman581@gmail.com>
2025-05-06 16:23:25 +0300

    replace Result with throw exceptions in Register Presenter  & it's test cases

--- src/main/kotlin/presentation/presenter/RegisterPresenter.kt
+++ src/main/kotlin/presentation/presenter/RegisterPresenter.kt
@@ -1,6 +1,5 @@
 package org.example.presentation.presenter
 
-/*
 import org.example.domain.model.RoleType
 import org.example.domain.model.User
 import org.example.domain.usecase.auth.RegisterUseCase
@@ -8,7 +7,9 @@ import org.example.domain.usecase.auth.RegisterUseCase
 class RegisterPresenter(
     private val registerUseCase: RegisterUseCase
 ) {
-    fun register(username: String, password: String, role: RoleType): Result<User> {
-        TODO("Not implement yet")
-    }
-}*/
+    fun register(username: String, password: String, role: RoleType): User = registerUseCase.register(
+        username = username ,
+        password = password ,
+        userRole = role
+    )
+}
