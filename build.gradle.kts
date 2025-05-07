0ce405d1e90931e058917cd361626c2fcff780a1
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-05-07 20:55:20 +0300

    add mongodb dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation("io.insert-koin:koin-core:4.0.3")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+
+    // Kotlin coroutine dependency
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    // For Kotlin driver
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.10.2")
+
+    testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0-M1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     testImplementation("io.mockk:mockk:1.13.16")
