b7c23835654bac03949e4665848bc989c8827a8d
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-05-08 07:22:12 +0300

    implement MongoAuditRepositoryImpl.kt

--- src/main/kotlin/data/mongorepository/MongoAuditRepositoryImpl.kt
+++ src/main/kotlin/data/mongorepository/MongoAuditRepositoryImpl.kt
@@ -1,34 +1,72 @@
 package data.mongorepository
 
+import com.mongodb.client.model.Filters.eq
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.toList
 import org.example.data.MongoCollections
+import org.example.domain.exception.EiffelFlowException
 import org.example.domain.model.AuditLog
 import org.example.domain.repository.AuditRepository
 import org.example.domain.repository.TaskRepository
 import java.util.UUID
 
 class MongoAuditRepositoryImpl(
-    private val database: MongoDatabase,
-    private val taskRepositoryProvider: Lazy<TaskRepository>
+    database: MongoDatabase,
+    taskRepositoryProvider: Lazy<TaskRepository>
 ) : AuditRepository {
 
     private val taskRepository: TaskRepository by taskRepositoryProvider
     private val auditLogsCollection = database.getCollection<AuditLog>(collectionName = MongoCollections.AUDIT_LOGS)
 
-
     override suspend fun createAuditLog(auditLog: AuditLog): AuditLog {
-        TODO("Not yet implemented")
+        try {
+            val existingLog = auditLogsCollection.find(eq("auditId", auditLog.auditId)).firstOrNull()
+            if (existingLog != null) {
+                throw EiffelFlowException.IOException("AuditLog with auditId ${auditLog.auditId} already exists")
+            }
+            auditLogsCollection.insertOne(auditLog)
+            return auditLog
+        } catch (exception: Throwable) {
+            throw EiffelFlowException.IOException("Can't create AuditLog because ${exception.message}")
+        }
     }
 
     override suspend fun getTaskAuditLogById(taskId: UUID): List<AuditLog> {
-        TODO("Not yet implemented")
+        return findAuditLogsByItemId(taskId)
     }
 
     override suspend fun getProjectAuditLogById(projectId: UUID): List<AuditLog> {
-        TODO("Not yet implemented")
+        val projectLogs = findAuditLogsByItemId(projectId)
+        val auditLogsForProjectTasks = getAuditProjectTasks(projectId, getAuditLogs())
+
+        return (projectLogs + auditLogsForProjectTasks).sortedByDescending { it.auditTime }
+    }
+
+    private suspend fun findAuditLogsByItemId(itemId: UUID): List<AuditLog> {
+        return try {
+            auditLogsCollection.find(eq("itemId", itemId))
+                .toList()
+        } catch (exception: Throwable) {
+            throw EiffelFlowException.IOException("Unexpected error fetching audit logs ${exception.message}")
+        }
+    }
+
+    private suspend fun getAuditProjectTasks(
+        projectId: UUID, auditLogs: List<AuditLog>
+    ): List<AuditLog> {
+        val tasksResult = taskRepository.getTasks()
+        val projectTaskIds = tasksResult.filter { it.projectId == projectId }.map { it.taskId }.toSet()
+
+        val auditLogsForProjectTasks = auditLogs.filter { projectTaskIds.contains(it.itemId) }
+        return auditLogsForProjectTasks
     }
 
     override suspend fun getAuditLogs(): List<AuditLog> {
-        TODO("Not yet implemented")
+        try {
+            return auditLogsCollection.find().toList()
+        } catch (exception: Throwable) {
+            throw EiffelFlowException.IOException("Can't get AuditLogs because ${exception.message}")
+        }
     }
 }
