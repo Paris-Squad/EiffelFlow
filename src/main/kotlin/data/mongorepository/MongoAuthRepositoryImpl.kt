b98e976a374a2c146c4b6c6d1019d46ae98e1fc1
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-05-08 19:57:12 +0300

    implement MongoAuthRepositoryImpl.kt

--- src/main/kotlin/data/mongorepository/MongoAuthRepositoryImpl.kt
+++ src/main/kotlin/data/mongorepository/MongoAuthRepositoryImpl.kt
@@ -1,7 +1,12 @@
 package data.mongorepository
 
+import com.mongodb.client.model.Filters.and
+import com.mongodb.client.model.Filters.eq
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import kotlinx.coroutines.flow.firstOrNull
+import org.bson.Document
 import org.example.data.MongoCollections
+import org.example.domain.exception.EiffelFlowException
 import org.example.domain.model.User
 import org.example.domain.repository.AuthRepository
 
@@ -10,20 +15,45 @@ class MongoAuthRepositoryImpl(
 ) : AuthRepository {
 
     private val authCollection = database.getCollection<User>(collectionName = MongoCollections.AUTH)
+    private val usersCollection = database.getCollection<User>(collectionName = MongoCollections.USERS)
 
     override suspend fun saveUserLogin(user: User): User {
-        TODO("Not yet implemented")
+        try {
+            val existingUser = authCollection.find(eq("userId", user.userId)).firstOrNull()
+            if (existingUser != null) {
+                throw EiffelFlowException.IOException("User with userId ${user.userId} already exists")
+            }
+            authCollection.insertOne(user)
+            return user
+        } catch (exception: Throwable) {
+            throw EiffelFlowException.IOException("Can't save user login because ${exception.message}")
+        }
     }
 
     override suspend fun isUserLoggedIn(): Boolean {
-        TODO("Not yet implemented")
+       authCollection.find().firstOrNull() ?: throw EiffelFlowException.NotFoundException(
+            "User is not logged in"
+        )
+        return true
     }
 
     override suspend fun clearLogin() {
-        TODO("Not yet implemented")
+        authCollection.deleteMany(Document())
     }
 
     override suspend fun loginUser(username: String, password: String): User {
-        TODO("Not yet implemented")
+        try {
+            val existUser = usersCollection.find(
+                and(
+                    eq("username", username),
+                    eq("password", password)
+                )
+            ).firstOrNull()
+
+            existUser ?: throw EiffelFlowException.NotFoundException("Invalid username or password")
+            return saveUserLogin(existUser)
+        } catch (throwable: Throwable) {
+            throw EiffelFlowException.IOException("Can't login user because ${throwable.message}")
+        }
     }
 }
