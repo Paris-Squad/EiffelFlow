c2961d098fce654700ab1ca19e083780d0aaf37e
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-05-12 00:44:33 +0300

    enhance the database Injection

--- src/main/kotlin/di/MongoModule.kt
+++ src/main/kotlin/di/MongoModule.kt
@@ -2,13 +2,11 @@ package org.example.di
 
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
-import org.example.data.remote.MongoCollections
-import org.example.data.remote.DataBaseConfigs
+import org.example.data.remote.MongoDatabaseProvider
+import org.example.data.remote.MongoClientProvider
 import org.koin.dsl.module
 
 val dataBaseModule = module {
-    single { MongoCollections }
-    single { DataBaseConfigs() }
-    single<MongoClient> { get<DataBaseConfigs>().getMongoClient() }
-    single<MongoDatabase> { get<DataBaseConfigs>().getDatabase(mongoClient = get()) }
+    single<MongoClient> { MongoClientProvider().client }
+    single<MongoDatabase> { MongoDatabaseProvider(get()).database }
 }
